b0ea9cd19118c0af4ad8d1ef301cb719701d2b61
nanoandrew4 <nanoandrew4@gmail.com>
2019-04-27 21:20:36 +0200

    Changed return type in ProductService from Product[] to List<Product>, now the code compiles.

--- src/main/java/com/openclassrooms/shop/service/ProductService.java
+++ src/main/java/com/openclassrooms/shop/service/ProductService.java
@@ -1,15 +1,14 @@
 package com.openclassrooms.shop.service;
 
-import com.openclassrooms.shop.domain.Cart;
-import com.openclassrooms.shop.domain.CartLine;
-import com.openclassrooms.shop.repository.OrderRepository;
 import com.openclassrooms.shop.domain.Product;
+import com.openclassrooms.shop.repository.OrderRepository;
 import com.openclassrooms.shop.repository.ProductRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -29,11 +28,9 @@ public class ProductService {
 	/**
 	 * @return all products from the inventory
 	 */
-	public Product[] getAllProducts() {
+	public List<Product> getAllProducts() {
 
-		// TODO change the return type from array to List<T> and propagate the change
-		// throughout the application
-		return productRepository.findAll();
+		return Arrays.asList(productRepository.findAll());
 	}
 
 	/**
