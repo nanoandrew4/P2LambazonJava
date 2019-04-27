df9b54aed845f015a9c18609d6966df9244ddd9c
nanoandrew4 <nanoandrew4@gmail.com>
2019-04-27 22:20:39 +0200

    Implemented addItem() and modified the Cart class, since getCartLines() made no sense as it was implemented. The cart line list was made a private class variable. The addItem test now passes.

--- src/main/java/com/openclassrooms/shop/domain/Cart.java
+++ src/main/java/com/openclassrooms/shop/domain/Cart.java
@@ -2,81 +2,88 @@ package com.openclassrooms.shop.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.function.Predicate;
 
 public class Cart {
+	private List<CartLine> cartLineList;
 
-    /**
-     *
-     * @return the actual cartline list
-     */
-    public List<CartLine> getCartLineList() {
-        return new ArrayList<>();
-    }
+	public Cart() {
+		cartLineList = new ArrayList<>();
+	}
 
-    /**
-     * Adds a getProductById in the cart or increment its quantity in the cart if already added
-     * @param product getProductById to be added
-     * @param quantity the quantity
-     */
-    public void addItem(Product product, int quantity) {
-        // TODO implement the method
-    }
+	/**
+	 * @return the actual cartline list
+	 */
+	public List<CartLine> getCartLineList() {
+		return cartLineList;
+	}
 
-    /**
-     * Removes a getProductById form the cart
-     * @param product the getProductById to be removed
-     */
-    public void removeLine(Product product) {
-        getCartLineList().removeIf(l -> l.getProduct().getId().equals(product.getId()));
-    }
+	/**
+	 * Adds a product to the cart or increments its quantity in the cart if it has already been added.
+	 *
+	 * @param product  The product to add to the cart
+	 * @param quantity The quantity of the product that is desired. If the product already exists in the cart,
+	 *                 this quantity will be added to the existing quantity
+	 */
+	public void addItem(Product product, int quantity) {
+		final Predicate<CartLine> filterByProductId = cl -> cl.getProduct().getId().longValue() == product.getId().longValue();
+		final CartLine cartLine = getCartLineList().stream().filter(filterByProductId).findFirst().orElse(null);
+
+		if (cartLine == null && quantity > 0)
+			getCartLineList().add(new CartLine(product, quantity));
+		else if (quantity > 0)
+			cartLine.setQuantity(cartLine.getQuantity() + quantity);
+	}
+
+	/**
+	 * Removes a getProductById form the cart
+	 *
+	 * @param product the getProductById to be removed
+	 */
+	public void removeLine(Product product) {
+		getCartLineList().removeIf(l -> l.getProduct().getId().equals(product.getId()));
+	}
 
 
-    /**
-     * @return total value of a cart
-     */
-    public double getTotalValue()
-    {
-         //TODO implement the method
-        return 0.0;
+	/**
+	 * @return total value of a cart
+	 */
+	public double getTotalValue() {
+		//TODO implement the method
+		return 0.0;
 
-    }
+	}
 
-    /**
-     * @return Get average value of a cart
-     */
-    public double getAverageValue()
-    {
-        // TODO implement the method
-        return 0.0;
-    }
+	/**
+	 * @return Get average value of a cart
+	 */
+	public double getAverageValue() {
+		// TODO implement the method
+		return 0.0;
+	}
 
-    /**
-     * @param productId the getProductById id to search for
-     * @return getProductById in the cart if it finds it
-     */
-    public Product findProductInCartLines(Long productId)
-    {
-        // TODO implement the method
-        return null;
-    }
+	/**
+	 * @param productId the getProductById id to search for
+	 * @return getProductById in the cart if it finds it
+	 */
+	public Product findProductInCartLines(Long productId) {
+		// TODO implement the method
+		return null;
+	}
 
-    /**
-     *
-     * @param index index of the cartLine
-     * @return CartLine in that index
-     */
-    public CartLine getCartLineByIndex(int index)
-    {
-        return getCartLineList().get(index);
-    }
+	/**
+	 * @param index index of the cartLine
+	 * @return CartLine in that index
+	 */
+	public CartLine getCartLineByIndex(int index) {
+		return getCartLineList().get(index);
+	}
 
-    /**
-     * Clears a the cart of all added products
-     */
-    public void clear()
-    {
-        List<CartLine> cartLines = getCartLineList();
-        cartLines.clear();
-    }
+	/**
+	 * Clears a the cart of all added products
+	 */
+	public void clear() {
+		List<CartLine> cartLines = getCartLineList();
+		cartLines.clear();
+	}
 }
